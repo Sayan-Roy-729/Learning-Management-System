f2e49ea1c0831deef20c89af711e6b2ae481aef8
Sayan Roy <rsayan553@gmail.com>
2021-04-28 12:03:25 +0530

    firebase authentication logic implemented

--- front-end/src/components/Signin/index.js
+++ front-end/src/components/Signin/index.js
@@ -1,48 +1,128 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form, FormButton, FormContent, FormH1, FormInput, FormLabel, FormWrap , Text , Icon, Container} from './SigninElements';
+import Swal from 'sweetalert2';
 
-import { createNewUser } from '../../actions/authAction';
+import { Form, FormButton, FormContent, FormH1, FormInput, FormLabel, FormWrap , Text , Icon, Container} from './SigninElements';
+import { signInUser, clearErrorMessage, userResetPassword, clearSentEmail } from '../../actions/authAction';
 import Loader from '../Loader/Loader';
 
 const SignIn = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [forgotPassword, setForgotPassword] = useState(false);
 
+    // ? Access the routing details
+    const history = useHistory();
+
+    // ? Access the redux
     const authenticationState = useSelector(state => state.authReducer);
     const dispatch = useDispatch();
 
-    const formSubmitHandler = event => {
-        event.preventDefault();
+    // ? If any error occurred during the sign in/reset password process, show alert
+    useEffect(() => {
+        if (authenticationState.sendMail && authenticationState.errorMessage) {
+            showAlertDialogBox(
+                'Failed to send an email',
+                authenticationState.errorMessage,
+                'error',
+                'red',                
+            );
+            
+            // After displaying the error dialog, clear from the global state
+            dispatch(clearSentEmail());
+        } else if (authenticationState.errorMessage) {
+            showAlertDialogBox(
+                'SignIn Failed!',
+                authenticationState.errorMessage,
+                'error',
+                'red',
+            );
 
-        dispatch(createNewUser(email, password));
-    };
+            //After displaying the error message, clear the error message state
+            dispatch(clearErrorMessage());
+        }
+    }, [authenticationState.errorMessage, dispatch, authenticationState.sendMail]);
 
-    
-        if (authenticationState.loading) {
-            return <Loader />;
-        } else {
-            return (
-                <>
-                    <Container>
-                        <FormWrap>
-                            <Icon to='/'>CampusX</Icon>
-                        <FormContent>
-                            <Form action='#' onSubmit = {event => formSubmitHandler(event)}>
-                                <FormH1>Sign in to your account</FormH1>
-                                <FormLabel htmlFor='for'>Email</FormLabel>
-                                <FormInput type='email' required value = {email} onChange = {(event) => setEmail(event.target.value)}/>
-                                <FormLabel htmlFor='for'>Password</FormLabel>
-                                <FormInput type='password' required value = {password} onChange = {(event) => setPassword(event.target.value)}/>
-                                <FormButton type='submit'>Continue</FormButton>
-                                <Text>Forgot password</Text>
-                            </Form>
-                        </FormContent>
-                        </FormWrap>
-                    </Container>
-                </>
+    // ? Show email sent dialog box to reset password
+    useEffect(() => {
+        if (authenticationState.sendMail) {
+            showAlertDialogBox(
+                'Email Send',
+                'An email is send to the email address. Please reset your password from the email',
+                'success',
+                'green',
             );
         }
+        // After displaying the info message, clear the sent mail state
+        dispatch(clearSentEmail());
+    }, [authenticationState.sendMail, dispatch]);
+
+    // ? After successful sign in, redirect to home page
+    useEffect(() => {
+        if (authenticationState.user) {
+            history.replace('/');
+        }
+    }); 
+
+
+    // ? Alert dialog box
+    const showAlertDialogBox = (title, text, icon, buttonColor) => {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: icon,
+            confirmButtonText: 'OK',
+            confirmButtonColor: buttonColor,
+        });
+    };
+
+    // ? Sign in Handler
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        if (!forgotPassword) {
+            // Dispatch for sign in
+            dispatch(signInUser(email, password));
+        } else {
+            // Dispatch for forgot password
+            dispatch(userResetPassword(email));
+        }
+    };
+    
+    if (authenticationState.loading) {
+        return <Loader />;
+    } else {
+        return (
+            <>
+                <Container>
+                    <FormWrap>
+                        <Icon to='/'>CampusX</Icon>
+                    <FormContent>
+                        <Form action='#' onSubmit = {event => formSubmitHandler(event)}>
+                            <FormH1>Sign in to your account</FormH1>
+                            <FormLabel htmlFor='for'>Email</FormLabel>
+                            <FormInput type='email' required value = {email} onChange = {(event) => setEmail(event.target.value)}/>
+                            {
+                                !forgotPassword && (
+                                    <div>
+                                        <FormLabel htmlFor='for'>Password</FormLabel>
+                                        <FormInput type='password' required value = {password} onChange = {(event) => setPassword(event.target.value)}/>
+                                    </div>
+                                )
+                            }
+                            <FormButton type='submit'>Continue</FormButton>
+                            {
+                                forgotPassword ? (
+                                    <Text onClick = {() => setForgotPassword(false)}>Return SignIn Page</Text>
+                                ) : <Text onClick = {() => setForgotPassword(true)}>Forgot password</Text>
+                            }
+                        </Form>
+                    </FormContent>
+                    </FormWrap>
+                </Container>
+            </>
+        );
+    }
 }
 
 export default SignIn
--- front-end/src/reducers/authReducer.js
+++ front-end/src/reducers/authReducer.js
@@ -1,13 +1,14 @@
-import { authConstants } from '../actions/constants'; 
+import { authConstants } from '../actions/constants';
 
 const initialState = {
     user: null,
     errorMessage: null,
     loading: false,
-}
+    sendMail: false,
+};
 
 const authReducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case authConstants.GET_CURRENT_USER_SIGN_IN:
             return {
                 ...state,
@@ -30,10 +31,90 @@ const authReducer = (state = initialState, action) => {
                 errorMessage: action.payload.errorMessage,
                 loading: false,
             };
-
+        case authConstants.CLEAR_ERROR_MESSAGE_STATE:
+            return {
+                ...state,
+                errorMessage: null,
+            };
+        case authConstants.USER_SIGN_IN_LOADING:
+            return {
+                ...state,
+                loading: true,
+            };
+        case authConstants.USER_SIGN_IN_SUCCESSFUL:
+            return {
+                ...state,
+                loading: false,
+                user: action.payload.user,
+            };
+        case authConstants.USER_SIGN_IN_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                errorMessage: action.payload.errorMessage,
+            };
+        case authConstants.USER_SIGN_OUT_LOADING:
+            return {
+                ...state,
+                loading: true,
+            };
+        case authConstants.USER_SIGN_OUT_SUCCESSFUL:
+            return {
+                ...state,
+                loading: false,
+                user: null,
+            };
+        case authConstants.USER_SIGN_OUT_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                errorMessage: action.payload.errorMessage,
+            };
+        case authConstants.USER_RESET_PASSWORD_BY_MAIL_LOADING:
+            return {
+                ...state,
+                loading: true,
+            };
+        case authConstants.USER_RESET_PASSWORD_BY_MAIL_SUCCESSFUL:
+            return {
+                ...state,
+                loading: false,
+                user: null,
+                sendMail: true,
+            };
+        case authConstants.USER_RESET_PASSWORD_BY_MAIL_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                errorMessage: action.payload.errorMessage,
+                sendMail: true,
+            };
+        case authConstants.USER_RESET_PASSWORD_BY_MAIL_CLEAR_STATE:
+            return {
+                ...state,
+                errorMessage: null,
+                sendMail: false,
+            };
+        case authConstants.USER_GOOGLE_SIGN_IN_LOADING:
+            return {
+                ...state,
+                loading: true,
+            };
+        case authConstants.USER_GOOGLE_SIGN_IN_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                user: action.payload.user,
+            };
+        case authConstants.USER_GOOGLE_SIGN_IN_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                errorMessage: action.payload.errorMessage,
+            };
         default:
             return state;
     }
-}
+};
 
 export default authReducer;
