1f6fa627c0e6ef2453c75291f1c897d7baafe2f8
Sayan Roy <rsayan553@gmail.com>
2021-05-08 20:28:18 +0530

    sign up page error show

--- front-end/src/components/SignUp/SignUp.js
+++ front-end/src/components/SignUp/SignUp.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import sweetAlert from 'sweetalert2';
 
-import { createNewUser, googleSignIn } from '../../actions/authAction';
+import {
+    createNewUser,
+    googleSignIn,
+    clearErrorMessage,
+} from '../../actions/authAction';
 import Loader from '../Loader/Loader';
 import './SignUp.css';
 
@@ -30,65 +35,79 @@ const SignUp = (props) => {
         dispatch(googleSignIn());
     };
 
-    if (authState.loading) {
-        return <Loader />;
-    } else {
-        return (
-            <div  style={{ marginTop: '50px' }}>
-                <form
-                    onSubmit={(event) => signInHandler(event)}
-                    className="d-flex flex-column justify-content-center  align-items-center"
-                >
-                    <div>
-                        <label className="FormField__Label" htmlFor="email">
-                            E-Mail Address
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="FormField__Input"
-                            placeholder="Enter your email"
-                            name="email"
-                            value={email}
-                            onChange={(event) => setEmail(event.target.value)}
-                        />
-                    </div>
-
-                    <div className="FormField">
-                        <label className="FormField__Label" htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="FormField__Input"
-                            placeholder="Enter your password"
-                            name="password"
-                            value={password}
-                            onChange={(event) =>
-                                setPassword(event.target.value)
-                            }
-                        />
-                    </div>
-
-                    <div className="FormField d-flex flex-column justify-content-center align-items-center">
-                        <button
-                            className="FormField__Button mr-20"
-                            type="submit"
-                            onClick={(event) => signInHandler(event)}
-                        >
-                            Sign Up
-                        </button>
-                        <span style={{ margin: '0 5px' }}>OR</span>
-                        <button className="FormField__Button mr-20" onClick = {googleSignInHandler}>
-                            SignIn with Google
-                        </button>
-                        {/*<Link to="/sign-in" className="FormField__Link">I'm already member</Link>*/}
-                    </div>
-                </form>
-            </div>
-        );
+    if (authState.errorMessage) {
+        sweetAlert
+            .fire({
+                title: 'Sign Up Error',
+                text: authState.errorMessage,
+                icon: 'error',
+                confirmButtonText: 'OK',
+                confirmButtonColor: 'red',
+            })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    dispatch(clearErrorMessage());
+                    history.push('/signin');
+                }
+            });
     }
+
+    return (
+        <div style={{ marginTop: '50px' }}>
+            <form
+                onSubmit={(event) => signInHandler(event)}
+                className="d-flex flex-column justify-content-center  align-items-center"
+            >
+                <div>
+                    <label className="FormField__Label" htmlFor="email">
+                        E-Mail Address
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="FormField__Input"
+                        placeholder="Enter your email"
+                        name="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+
+                <div className="FormField">
+                    <label className="FormField__Label" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="FormField__Input"
+                        placeholder="Enter your password"
+                        name="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+
+                <div className="FormField d-flex flex-column justify-content-center align-items-center">
+                    <button
+                        className="FormField__Button mr-20"
+                        type="submit"
+                        onClick={(event) => signInHandler(event)}
+                    >
+                        Sign Up
+                    </button>
+                    <span style={{ margin: '0 5px' }}>OR</span>
+                    <button
+                        className="FormField__Button mr-20"
+                        onClick={googleSignInHandler}
+                    >
+                        SignIn with Google
+                    </button>
+                    {/*<Link to="/sign-in" className="FormField__Link">I'm already member</Link>*/}
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default SignUp;
--- front-end/src/pages/signup.js
+++ front-end/src/pages/signup.js
@@ -1,25 +1,34 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import HeroSection from '../components/HeroSection';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import SignUp from '../components/SignUp/SignUp';
+import Loader from '../components/Loader/Loader';
 
-const SignUpPage = props => {
+const SignUpPage = (props) => {
     const [isOpen, setIsOpen] = useState(false);
+    const authState = useSelector((state) => state.authReducer);
+
     const toggle = () => {
-        setIsOpen(oldState => {
+        setIsOpen((oldState) => {
             return !oldState;
         });
     };
-    return (
-        <>  
-            <Sidebar isOpen = {isOpen} toggle = {toggle}/>
-            <Navbar toggle = {toggle}/>
-            <HeroSection />
-            <SignUp />
-        </>
-    );
+
+    if (authState.loading) {
+        return <Loader />;
+    } else {
+        return (
+            <>
+                <Sidebar isOpen={isOpen} toggle={toggle} />
+                <Navbar toggle={toggle} />
+                <HeroSection />
+                <SignUp />
+            </>
+        );
+    }
 };
 
 export default SignUpPage;
